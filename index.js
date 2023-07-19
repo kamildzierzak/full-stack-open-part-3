6343fa4dfc7dc3dbd786bef07d9ce0c3d153fe61
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-19 19:42:17 +0200

    add 3.5: Phonebook backend step5 solution

--- index.js
+++ index.js
@@ -55,6 +55,37 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateRandomId = () => {
+  const randomId = Math.floor(Math.random() * 10000000)
+  return randomId
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name is missing',
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing',
+    })
+  }
+
+  const person = {
+    id: generateRandomId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
